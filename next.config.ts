779d069710f1a69d36ad97df1772d87cb3ce7f3a
TheUnforgivenOne <vladpotapow@yandex.ru>
2025-04-18 14:34:02 +0200

    Base path for the project build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from 'next';
-// import { name as projectName } from './package.json';
+import { name as projectName } from './package.json';
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  // basePath: `/${projectName}`,
+  basePath: `/${projectName}`,
   // assetPrefix: `/${projectName}/`,
 };
 
