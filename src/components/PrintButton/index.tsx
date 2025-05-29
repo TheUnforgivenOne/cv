9d12edbe8beed6387267c8a63928308869b5a41a
TheUnforgivenOne <vladpotapow@yandex.ru>
2025-05-29 14:31:37 +0200

    Print button text localization

--- src/components/PrintButton/index.tsx
+++ src/components/PrintButton/index.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react';
+import { getTranslation } from '@/i18n';
 import styles from './printButton.module.css';
 
 interface PrintButtonProps {
   lang: string;
 }
 
-const PrintButton: FC<PrintButtonProps> = ({ lang }) => {
+const PrintButton: FC<PrintButtonProps> = async ({ lang }) => {
+  const { t } = await getTranslation(lang);
+
   return (
     <a href={`CV_Vladislav_Potapov_${lang}.pdf`} download className={styles.printButton}>
-      Save as PDF
+      {t('printButtonText')}
     </a>
   );
 };
